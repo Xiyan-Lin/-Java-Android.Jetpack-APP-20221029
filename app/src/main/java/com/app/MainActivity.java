236cb608278d8c360a89b9eb057ad1dfb47c946e
MB-teacher <vincentjava@yahoo.com.tw>
2022-10-29 14:02:29 +0800

    android

--- app/src/main/java/com/app/MainActivity.java
+++ app/src/main/java/com/app/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+// 主程式
 public class MainActivity extends AppCompatActivity {
 
+    // 程式進入點
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 配置 UI 畫面
         setContentView(R.layout.activity_main);
     }
 }
