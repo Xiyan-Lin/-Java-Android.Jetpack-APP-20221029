f61807f2b86a0db14d0cde06dd4f57e26847c912
MB-teacher <vincentjava@yahoo.com.tw>
2022-11-05 15:04:43 +0800

    android

--- app_layout_login/src/main/java/com/example/app_layout_login/MainActivity.java
+++ app_layout_login/src/main/java/com/example/app_layout_login/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.app_layout_login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Bra");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { // 建立 options menu
+        // 將 options_menu.xml 充氣為物件
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) { // 按下 options menu's item
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
