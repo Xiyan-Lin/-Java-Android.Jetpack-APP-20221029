48c5161e8fa7bf9e26ab6906f73b2e167c596864
MB-teacher <vincentjava@yahoo.com.tw>
2022-11-05 14:42:55 +0800

    android

--- app_layout_login/src/main/java/com/example/app_layout_login/LoginActivity.java
+++ app_layout_login/src/main/java/com/example/app_layout_login/LoginActivity.java
@@ -2,13 +2,23 @@ package com.example.app_layout_login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private Button loginBtn;
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        context = this;
+        loginBtn = findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener((view) -> {
+            Intent intent = new Intent(context, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
