0885750ac49f6628879cdf34d83e582704f40776
MB-teacher <vincentjava@yahoo.com.tw>
2022-11-05 15:32:11 +0800

    android

--- app_layout_login/src/main/java/com/example/app_layout_login/MainActivity.java
+++ app_layout_login/src/main/java/com/example/app_layout_login/MainActivity.java
@@ -10,10 +10,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
     private CircleImageView braImageView;
+    private BottomNavigationView bottomNavMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
         setTitle("Bra");
         braImageView = findViewById(R.id.bra_iv);
         registerForContextMenu(braImageView); // 註冊 context menu
+        bottomNavMenu = findViewById(R.id.bottom_nav_menu);
+        // 監聽 bottomNavMenu
+        bottomNavMenu.setOnNavigationItemSelectedListener((item) -> {
+            Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            return true; // 事件到此結束, 不會往下傳遞
+        });
     }
 
     @Override
