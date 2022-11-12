954697d98547e3785acfdcbf88826a09a4ec830b
MB-teacher <vincentjava@yahoo.com.tw>
2022-11-12 14:59:09 +0800

    android

--- app_room_dice/src/main/java/com/example/app_room_dice/adapter/DiceAdapter.java
+++ app_room_dice/src/main/java/com/example/app_room_dice/adapter/DiceAdapter.java
@@ -47,6 +47,31 @@ public class DiceAdapter extends BaseAdapter {
     public View getView(int i, View convertView, ViewGroup viewGroup) {
         // Layout 配置
         LayoutInflater layoutInflater = LayoutInflater.from(context);
+        ViewHolder holder;
+        if(convertView == null) {
+            holder = new ViewHolder();
+            convertView = layoutInflater.inflate(R.layout.list_item, null);
+            // 將 ui 結構全部放到 holder 物件中
+            holder.tvId = convertView.findViewById(R.id.tv_id);
+            holder.tvD1 = convertView.findViewById(R.id.tv_d1);
+            holder.tvD2 = convertView.findViewById(R.id.tv_d2);
+            holder.tvD3 = convertView.findViewById(R.id.tv_d3);
+            holder.tvSum = convertView.findViewById(R.id.tv_sum);
+            // 放到 convertView 中保存
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        // 將資料逐筆配置到 UI 上
+        Dice dice = diceList.get(i);
+        holder.tvId.setText(dice.getId() + "");
+        setDiceImage(holder.tvD1, dice.getD1());
+        setDiceImage(holder.tvD2, dice.getD2());
+        setDiceImage(holder.tvD3, dice.getD3());
+        holder.tvSum.setText(dice.getSum() + "");
+
+        /*
+        // 舊有做法
         // 將 R.layout.list_item 物件化
         convertView = layoutInflater.inflate(R.layout.list_item, null);
         TextView tvId = convertView.findViewById(R.id.tv_id);
@@ -61,6 +86,7 @@ public class DiceAdapter extends BaseAdapter {
         setDiceImage(tvD2, dice.getD2());
         setDiceImage(tvD3, dice.getD3());
         tvSum.setText(dice.getSum() + "");
+        */
         return convertView;
     }
 
@@ -68,5 +94,14 @@ public class DiceAdapter extends BaseAdapter {
         int[] diceImages = {R.drawable.dice1, R.drawable.dice2, R.drawable.dice3,
                             R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
         imageView.setImageResource(diceImages[number-1]);
+    }
+
+    // 優化 ListView 避免重新加載, 提高呈現效能
+    static class ViewHolder {
+        TextView tvId;
+        ImageView tvD1;
+        ImageView tvD2;
+        ImageView tvD3;
+        TextView tvSum;
     }
 }
