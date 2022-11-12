69f78fba0a111ac331bae2fbcea4874f3ac0b4fc
MB-teacher <vincentjava@yahoo.com.tw>
2022-11-12 12:27:44 +0800

    android

--- app_room_dice/src/main/java/com/example/app_room_dice/adapter/DiceAdapter.java
+++ app_room_dice/src/main/java/com/example/app_room_dice/adapter/DiceAdapter.java
@@ -1,10 +1,13 @@
 package com.example.app_room_dice.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
+import com.example.app_room_dice.R;
 import com.example.app_room_dice.entity.Dice;
 
 import java.util.List;
@@ -40,7 +43,23 @@ public class DiceAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+        // Layout 配置
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        // 將 R.layout.list_item 物件化
+        convertView = layoutInflater.inflate(R.layout.list_item, viewGroup);
+        TextView tvId = convertView.findViewById(R.id.tv_id);
+        TextView tvD1 = convertView.findViewById(R.id.tv_d1);
+        TextView tvD2 = convertView.findViewById(R.id.tv_d2);
+        TextView tvD3 = convertView.findViewById(R.id.tv_d3);
+        TextView tvSum = convertView.findViewById(R.id.tv_sum);
+        // 將資料逐筆配置到 UI 上
+        Dice dice = diceList.get(i);
+        tvId.setText(dice.getId() + "");
+        tvD1.setText(dice.getD1() + "");
+        tvD2.setText(dice.getD2() + "");
+        tvD3.setText(dice.getD3() + "");
+        tvSum.setText(dice.getSum() + "");
+        return convertView;
     }
 }
