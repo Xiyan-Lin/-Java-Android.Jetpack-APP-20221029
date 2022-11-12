bf1da1ec5ec6e2d824c4eac1579895d067bab6ab
MB-teacher <vincentjava@yahoo.com.tw>
2022-11-12 15:52:05 +0800

    android

--- app_fcm/src/main/java/com/example/app_fcm/MainActivity.java
+++ app_fcm/src/main/java/com/example/app_fcm/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.app_fcm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getFirebaseToken();
+    }
+
+    private void getFirebaseToken() {
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener((task -> {
+                    if(task.isSuccessful()) {
+                        String token = task.getResult(); // token
+                        Log.i("fcm", "get token: " + token);
+
+                    }
+                }));
     }
 }
