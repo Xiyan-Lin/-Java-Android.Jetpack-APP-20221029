0cf845b39fcb7a314d5bca8159110c58d593b05e
MB-teacher <vincentjava@yahoo.com.tw>
2022-11-12 10:39:45 +0800

    android

---
+++ app_room_dice/src/main/java/com/example/app_room_dice/room/DiceDatabase.java
@@ -0,0 +1,27 @@
+package com.example.app_room_dice.room;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.app_room_dice.dao.DiceDao;
+import com.example.app_room_dice.entity.Dice;
+
+@Database(entities = {Dice.class}, version = 1)
+public abstract class DiceDatabase extends RoomDatabase {
+    private static final String DATABASE_NAME = "dice_db"; // 資料庫存檔檔名
+    private static DiceDatabase diceDatabase;
+    private static synchronized DiceDatabase getInstance(Context context) {
+        if(diceDatabase == null) {
+            diceDatabase = Room
+                    .databaseBuilder(context, DiceDatabase.class, DATABASE_NAME)
+                    .build();
+        }
+        return diceDatabase;
+    }
+
+    public abstract DiceDao diceDao();
+
+}
