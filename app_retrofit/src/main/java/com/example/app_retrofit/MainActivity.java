1bc8c32b56cbf825e1876fa2907391cf3e88834f
MB-teacher <vincentjava@yahoo.com.tw>
2022-11-19 15:10:52 +0800

    android

--- app_retrofit/src/main/java/com/example/app_retrofit/MainActivity.java
+++ app_retrofit/src/main/java/com/example/app_retrofit/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.app_retrofit;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -31,12 +36,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = this;
+        Api api = RetrofitClient.getInstance().getApi();
 
         listView = findViewById(R.id.list_view);
         postAdapter = new PostAdapter(context);
         listView.setAdapter(postAdapter);
 
-        Api api = RetrofitClient.getInstance().getApi();
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long id) {
+                try {
+                    api.getPost((int) id).enqueue(new Callback<Post>() {
+                        @Override
+                        public void onResponse(Call<Post> call, Response<Post> response) {
+                            Post post = response.body();
+                            // 對話視窗設定/建構
+                            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                            builder.setTitle("修改 title");
+                            Log.i("retrofit", post.toString());
+                            // 自訂 EditText
+                            EditText et = new EditText(context);
+                            et.setText(post.title);
+                            builder.setView(et);
+                            builder.setPositiveButton("修改", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+
+                                }
+                            });
+                            builder.setNegativeButton("離開", null);
+                            // 建立對話視窗
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+                        }
+
+                        @Override
+                        public void onFailure(Call<Post> call, Throwable t) {
+
+                        }
+                    });
+
+                } catch (Exception e) {
+                }
+                return true;
+            }
+        });
+
+
         SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.swipeRefresh);
         swipeRefreshLayout.setOnRefreshListener(() -> {
             api.getPosts().enqueue(new Callback<List<Post>>() {
