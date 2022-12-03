6c263abd1f4a9589302ef88311081e8d0b4260af
MB-teacher <vincentjava@yahoo.com.tw>
2022-12-03 12:08:33 +0800

    android

--- app_viewmodel/src/main/java/com/example/app_viewmodel/MyViewModel.java
+++ app_viewmodel/src/main/java/com/example/app_viewmodel/MyViewModel.java
@@ -1,16 +1,26 @@
 package com.example.app_viewmodel;
 
+import android.app.Application;
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.ViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
-// ViewModel vs AndroidViewModel(會自行帶入 Context 變數)
-public class MyViewModel extends ViewModel {
+// ViewModel vs AndroidViewModel(會自行帶入 Application 變數)
+//public class MyViewModel extends ViewModel {
+public class MyViewModel extends AndroidViewModel {
+
     private List<Bmi> bmiList = new ArrayList<>();
+    private Context context;
+
+    public MyViewModel(@NonNull Application application) {
+        super(application);
+        context = application.getApplicationContext();
+    }
 
     public List<Bmi> getBmiList() {
         return bmiList;
