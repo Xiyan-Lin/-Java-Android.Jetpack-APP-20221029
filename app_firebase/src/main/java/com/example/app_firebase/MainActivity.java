bd0ff29a43fae4a0034695584a6dc24ab0a169b0
MB-teacher <vincentjava@yahoo.com.tw>
2022-12-03 14:13:17 +0800

    android

--- app_firebase/src/main/java/com/example/app_firebase/MainActivity.java
+++ app_firebase/src/main/java/com/example/app_firebase/MainActivity.java
@@ -6,9 +6,17 @@ import android.content.Context;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
     private TextView tvNumber;
     private Context context;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("number");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +25,22 @@ public class MainActivity extends AppCompatActivity {
         context = this;
         tvNumber = findViewById(R.id.tv_number);
 
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                tvNumber.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+            }
+        });
+
     }
+
 }
