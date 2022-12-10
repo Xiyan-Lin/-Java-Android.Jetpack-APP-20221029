12b68fd62430d898fd2bf92ac8c4600f937940f6
MB-teacher <vincentjava@yahoo.com.tw>
2022-12-10 14:12:02 +0800

    android

---
+++ app_room_livedata_viewmodel/src/main/java/com/example/app_room_livedata_viewmodel/database/MyDatabase.java
@@ -0,0 +1,25 @@
+package com.example.app_room_livedata_viewmodel.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.app_room_livedata_viewmodel.entity.Student;
+
+@Database(entities = {Student.class}, version = 1)
+public abstract class MyDatabase extends RoomDatabase {
+    public static final String DATABASE_NAME = "my_db";
+    private static MyDatabase databaseInstance;
+    public static synchronized MyDatabase getInstance(Context context) {
+        if(databaseInstance == null) {
+                databaseInstance = Room
+                        .databaseBuilder(context.getApplicationContext(), MyDatabase.class, DATABASE_NAME)
+                        .build();
+        }
+        return databaseInstance;
+    }
+
+    public abstract StudentDao studentDao();
+}
