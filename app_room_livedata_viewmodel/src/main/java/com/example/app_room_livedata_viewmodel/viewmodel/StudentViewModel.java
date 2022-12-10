7116b22f78aed0ea6807cae7869d18dd23d2347d
MB-teacher <vincentjava@yahoo.com.tw>
2022-12-10 14:15:57 +0800

    android

---
+++ app_room_livedata_viewmodel/src/main/java/com/example/app_room_livedata_viewmodel/viewmodel/StudentViewModel.java
@@ -0,0 +1,26 @@
+package com.example.app_room_livedata_viewmodel.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.app_room_livedata_viewmodel.database.MyDatabase;
+import com.example.app_room_livedata_viewmodel.entity.Student;
+
+import java.util.List;
+
+public class StudentViewModel extends AndroidViewModel {
+    private MyDatabase myDatabase;
+    private LiveData<List<Student>> liveDataStudents;
+    public StudentViewModel(@NonNull Application application) {
+        super(application);
+        myDatabase = MyDatabase.getInstance(application);
+        liveDataStudents = myDatabase.studentDao().queryAll();
+    }
+
+    public LiveData<List<Student>> getLiveDataStudents() {
+        return liveDataStudents;
+    }
+}
