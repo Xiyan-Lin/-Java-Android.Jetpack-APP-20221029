8241582919d45af5881d733b680b0d8dd70624d8
MB-teacher <vincentjava@yahoo.com.tw>
2022-12-10 10:27:44 +0800

    android

--- app_livedata/src/main/java/com/example/app_livedata/MainActivity.java
+++ app_livedata/src/main/java/com/example/app_livedata/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.app_livedata;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tvTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvTime = findViewById(R.id.tvTime);
+        initComponent();
     }
+
+    private void initComponent() {
+        // 得到 ViewModel
+        TimerWithLiveDataViewModel timerWithLiveDataViewModel =
+                ViewModelProviders.of(this).get(TimerWithLiveDataViewModel.class);
+        // 得到 LiveData
+        MutableLiveData<Integer> liveData =
+                (MutableLiveData<Integer>)timerWithLiveDataViewModel.getCurrentSecond();
+        // 觀察數據變化
+        liveData.observe(this, (second) -> {
+            tvTime.setText(second + "");
+        });
+        // 開始計時
+        timerWithLiveDataViewModel.startTiming();
+    }
+
 }
