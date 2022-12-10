a744991bb9b1f835d2b7b5aaccc4deda75ad2975
MB-teacher <vincentjava@yahoo.com.tw>
2022-12-10 12:14:57 +0800

    android

---
+++ app_databinding/src/main/java/com/example/app_databinding/BookRatingUtil.java
@@ -0,0 +1,7 @@
+package com.example.app_databinding;
+
+public class BookRatingUtil {
+    public static String getRatingString(int rating) {
+        return rating + "顆星";
+    }
+}
--- app_databinding/src/main/java/com/example/app_databinding/MainActivity.java
+++ app_databinding/src/main/java/com/example/app_databinding/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.app_databinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingComponent;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+
+import com.example.app_databinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        Book book = new Book("Android", "John", 3);
+        activityMainBinding.setBook(book);
+        book.author = "Mary";
     }
 }
