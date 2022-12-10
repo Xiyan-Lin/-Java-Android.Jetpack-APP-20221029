76d52c398394434ea1e95e9559689da56a688d63
MB-teacher <vincentjava@yahoo.com.tw>
2022-12-10 14:21:06 +0800

    android

--- app_room_livedata_viewmodel/src/main/java/com/example/app_room_livedata_viewmodel/MainActivity.java
+++ app_room_livedata_viewmodel/src/main/java/com/example/app_room_livedata_viewmodel/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.app_room_livedata_viewmodel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Context;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.app_room_livedata_viewmodel.database.MyDatabase;
+import com.example.app_room_livedata_viewmodel.entity.Student;
+import com.example.app_room_livedata_viewmodel.viewmodel.StudentViewModel;
 
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private MyDatabase myDatabase;
+    private List<Student> students;
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+
+        myDatabase = MyDatabase.getInstance(context);
+        StudentViewModel studentViewModel = ViewModelProviders.of(this).get(StudentViewModel.class);
+        studentViewModel.getLiveDataStudents().observe(this, (students) -> {
+
+        });
     }
 }
