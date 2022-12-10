5dc8dad90f63afcb1f6767ab842ea2eff9d30842
MB-teacher <vincentjava@yahoo.com.tw>
2022-12-10 11:52:52 +0800

    android

---
+++ app_databinding/src/main/java/com/example/app_databinding/Book.java
@@ -0,0 +1,13 @@
+package com.example.app_databinding;
+
+public class Book {
+    public String title;
+    public String author;
+    public int rating;
+
+    public Book(String title, String author, int rating) {
+        this.title = title;
+        this.author = author;
+        this.rating = rating;
+    }
+}
