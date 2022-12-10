b6b197f0758dfe55f6b9f730727b1a8f62113d03
MB-teacher <vincentjava@yahoo.com.tw>
2022-12-10 11:14:38 +0800

    android

--- app_livedata_fragment/src/main/java/com/example/app_livedata_fragment/OneFragment.java
+++ app_livedata_fragment/src/main/java/com/example/app_livedata_fragment/OneFragment.java
@@ -10,6 +10,8 @@ import android.widget.SeekBar;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModelProviders;
 
 public class OneFragment extends Fragment {
 
@@ -19,10 +21,19 @@ public class OneFragment extends Fragment {
         View parentView = inflater.inflate(R.layout.fragment_one, container, false);
         SeekBar seekBar = parentView.findViewById(R.id.seekBar);
 
+        // 建立 ViewModel
+        ShareDataViewModel shareDataViewModel = ViewModelProviders.of(getActivity()).get(ShareDataViewModel.class);
+        // 建立 livedata
+        MutableLiveData<Integer> progressLiveData =( MutableLiveData<Integer>)shareDataViewModel.getProgress();
+        // 監聽
+        progressLiveData.observe(getActivity(), (progress) -> seekBar.setProgress(progress));
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                Log.i("MyLog", "Fragment One SeekBar value: " + i);
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                Log.i("MyLog", "Fragment One SeekBar value: " + progress);
+                // 變更 livedata 的資料
+                progressLiveData.setValue(progress);
             }
 
             @Override
