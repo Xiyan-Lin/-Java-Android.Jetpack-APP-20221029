ba280b79d9b8b8189736d4a68200d00e601a7aca
MB-teacher <vincentjava@yahoo.com.tw>
2022-12-17 14:16:03 +0800

    android

--- app_databinding_recyclerview_room/src/main/java/com/example/app_databinding_recyclerview_room/model/Student.java
+++ app_databinding_recyclerview_room/src/main/java/com/example/app_databinding_recyclerview_room/model/Student.java
@@ -1,6 +1,44 @@
 package com.example.app_databinding_recyclerview_room.model;
 
-@Entity
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "student")
 public class Student {
 
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id", typeAffinity = ColumnInfo.INTEGER)
+    public int id;
+
+    @ColumnInfo(name = "name", typeAffinity = ColumnInfo.TEXT)
+    public String name;
+
+    @ColumnInfo(name = "age", typeAffinity = ColumnInfo.INTEGER)
+    public int age;
+
+    // Room 會使用此建構子將表中的資料轉為 student 物件
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    // 此建構子只給 Java 使用不給 Room 使用(因為 Room 只能識別一種建構子)
+    // 用於 Java 新增資料
+    @Ignore
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
