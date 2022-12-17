ff9eebf1329fd83e5e06482a85f0f7833205a3ae
MB-teacher <vincentjava@yahoo.com.tw>
2022-12-17 14:54:39 +0800

    android

--- app_databinding_recyclerview_room/src/main/java/com/example/app_databinding_recyclerview_room/adapter/RecyclerViewAdapter.java
+++ app_databinding_recyclerview_room/src/main/java/com/example/app_databinding_recyclerview_room/adapter/RecyclerViewAdapter.java
@@ -1,29 +1,42 @@
 package com.example.app_databinding_recyclerview_room.adapter;
 
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.app_databinding_recyclerview_room.R;
 import com.example.app_databinding_recyclerview_room.databinding.LayoutItemBinding;
+import com.example.app_databinding_recyclerview_room.model.Student;
+
+import java.util.List;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+    private List<Student> students;
 
+    public RecyclerViewAdapter(List<Student> students) {
+        this.students = students;
+    }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutItemBinding layoutItemBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.getContext()), R.layout.layout_item, parent, false);
+        return new MyViewHolder(layoutItemBinding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        Student student = students.get(position);
+        holder.layoutItemBinding.setStudent(student);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return students.size();
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
