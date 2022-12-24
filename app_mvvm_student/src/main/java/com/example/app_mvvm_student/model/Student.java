64e900218c756211ad1590980c268a788ae103ae
MB-teacher <vincentjava@yahoo.com.tw>
2022-12-24 10:29:15 +0800

    android

--- app_mvvm_student/src/main/java/com/example/app_mvvm_student/model/Student.java
+++ app_mvvm_student/src/main/java/com/example/app_mvvm_student/model/Student.java
@@ -1,4 +1,42 @@
 package com.example.app_mvvm_student.model;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "student")
 public class Student {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id", typeAffinity = ColumnInfo.INTEGER)
+    public int id;
+
+    @ColumnInfo(name = "name", typeAffinity = ColumnInfo.TEXT)
+    public String name;
+
+    @ColumnInfo(name = "age", typeAffinity = ColumnInfo.INTEGER)
+    public int age;
+
+    // 給 room 進行 O/R Mapping 構造使用
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Ignore
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
