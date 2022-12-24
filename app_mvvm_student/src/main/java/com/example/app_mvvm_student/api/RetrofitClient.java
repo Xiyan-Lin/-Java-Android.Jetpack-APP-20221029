8d8fa57260991de4049506fa757f4b6caaa2dce0
MB-teacher <vincentjava@yahoo.com.tw>
2022-12-24 14:21:57 +0800

    android

--- app_mvvm_student/src/main/java/com/example/app_mvvm_student/api/RetrofitClient.java
+++ app_mvvm_student/src/main/java/com/example/app_mvvm_student/api/RetrofitClient.java
@@ -1,4 +1,29 @@
 package com.example.app_mvvm_student.api;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitClient {
+
+    private static final String BASE_URL = "http://192.168.1.157:3000/"; // http://10.0.2.2:3000/
+    private static RetrofitClient retrofitClient;
+    private Retrofit retrofit;
+
+    private RetrofitClient() {
+        retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static synchronized RetrofitClient getInstance() {
+        if(retrofitClient == null) {
+            retrofitClient = new RetrofitClient();
+        }
+        return retrofitClient;
+    }
+
+    public Api getApi() {
+        return retrofit.create(Api.class);
+    }
+
 }
