5eddccc72f078774dae73654a0af3d3f90c854a4
MB-teacher <vincentjava@yahoo.com.tw>
2022-12-24 16:23:01 +0800

    android

--- app_mvvm_student/src/main/java/com/example/app_mvvm_student/adapter/ItemImageBindingAdapter.java
+++ app_mvvm_student/src/main/java/com/example/app_mvvm_student/adapter/ItemImageBindingAdapter.java
@@ -13,7 +13,6 @@ public class ItemImageBindingAdapter {
     // 用來加載 app:itemImage="@{student.imageBase64}"
     @BindingAdapter("itemImage")
     public static void setImage(ImageView imageView, String imageBase64) {
-        Context context = imageView.getContext();
         byte[] decodedString = Base64.getDecoder().decode(imageBase64);
         Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
         imageView.setImageBitmap(bitmap);
