85974572be19ff79bd061ad1147d0c58ad276ba6
MB-teacher <vincentjava@yahoo.com.tw>
2022-12-24 11:11:31 +0800

    android

--- app_mvvm_student/src/main/java/com/example/app_mvvm_student/database/MyDatabase.java
+++ app_mvvm_student/src/main/java/com/example/app_mvvm_student/database/MyDatabase.java
@@ -1,4 +1,24 @@
 package com.example.app_mvvm_student.database;
 
-public class MyDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.app_mvvm_student.model.Student;
+
+@Database(entities = {Student.class}, version = 1)
+public abstract class MyDatabase extends RoomDatabase {
+    private static final String DATABASE_NAME = "my_db";
+    private static MyDatabase databaseInstance;
+    public static synchronized MyDatabase getInstance(Context context) {
+        if(databaseInstance == null) {
+            databaseInstance = Room.databaseBuilder(context.getApplicationContext(), MyDatabase.class, DATABASE_NAME)
+                    .build();
+        }
+        return databaseInstance;
+    }
+
+    public abstract StudentDao studentDao();
 }
--- app_mvvm_student/src/main/java/com/example/app_mvvm_student/database/StudentDao.java
+++ app_mvvm_student/src/main/java/com/example/app_mvvm_student/database/StudentDao.java
@@ -1,4 +1,37 @@
 package com.example.app_mvvm_student.database;
 
-public class StudentDao {
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.app_mvvm_student.model.Student;
+
+import java.util.List;
+
+@Dao
+public interface StudentDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Student student);
+
+    @Update
+    void update(Student student);
+
+    @Delete
+    void delete(Student student);
+
+    // 實現遠端與本地端的資料同步
+    @Query("DELETE FROM student where id not in (:ids)")
+    public void deleteNotInId(int[] ids);
+
+    @Query("SELECT id, name, age FROM student")
+    LiveData<List<Student>> queryAll();
+
+    @Query("SELECT id, name, age FROM student WHERE id = :id")
+    LiveData<Student> getById(int id);
+
 }
