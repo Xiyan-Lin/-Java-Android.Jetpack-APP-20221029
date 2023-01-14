a82200355fa6902b76e934d06fc8662a18364e4d
MB-teacher <vincentjava@yahoo.com.tw>
2023-01-14 15:26:21 +0800

    android

---
+++ app_paging_boundry/src/main/java/com/example/app_paging_boundry/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.app_paging_boundry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
