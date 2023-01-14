5feb7544be10c546d61c7f6c2522a6e2e99b6610
MB-teacher <vincentjava@yahoo.com.tw>
2023-01-14 12:15:44 +0800

    android

--- app_paging/src/main/java/com/example/app_paging/api/RetrofitClient.java
+++ app_paging/src/main/java/com/example/app_paging/api/RetrofitClient.java
@@ -1,4 +1,33 @@
 package com.example.app_paging.api;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitClient {
+
+    private static final String BASE_URL = "https://api.itbook.store/1.0/";
+    private static RetrofitClient retrofitClient;
+    private Retrofit retrofit;
+
+    private RetrofitClient() {
+        Gson gson = new GsonBuilder().create();
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+    }
+
+    public static synchronized RetrofitClient getApiClient() {
+        if(retrofitClient == null) {
+            retrofitClient = new RetrofitClient();
+        }
+        return retrofitClient;
+    }
+
+    public Api getApiInterface() {
+        return retrofit.create(Api.class);
+    }
 }
