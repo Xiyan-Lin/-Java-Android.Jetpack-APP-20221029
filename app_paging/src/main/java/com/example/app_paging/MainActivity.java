7ed8c4d007cc8092375f3ff47527490099484ef3
MB-teacher <vincentjava@yahoo.com.tw>
2023-01-14 14:25:41 +0800

    android

--- app_paging/src/main/java/com/example/app_paging/MainActivity.java
+++ app_paging/src/main/java/com/example/app_paging/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.app_paging;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+
+        // 在 Title 中顯示 icon
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.drawable.blue_circle_48);
+
+        EditText searchEdit = findViewById(R.id.books_edt);
+        Button searchBtn = findViewById(R.id.books_btn);
+
+        searchBtn.setOnClickListener(view -> {
+            String searchStr = searchEdit.getText().toString();
+            if(searchStr.isEmpty()) {
+                Toast.makeText(context, "Search box is empty!", Toast.LENGTH_SHORT).show();
+            } else {
+                Intent intent = new Intent(context, BookListActivity.class);
+                intent.putExtra("SEARCH TAG", searchStr);
+                startActivity(intent);
+            }
+        });
     }
 }
