224f8285595767f8d8aed826b5efe9845fc80e5a
MB-teacher <vincentjava@yahoo.com.tw>
2023-01-14 16:05:21 +0800

    android

--- app_paging_boundry/src/main/java/com/example/app_paging_boundry/paging/UserBoundaryCallback.java
+++ app_paging_boundry/src/main/java/com/example/app_paging_boundry/paging/UserBoundaryCallback.java
@@ -1,11 +1,27 @@
 package com.example.app_paging_boundry.paging;
 
+import android.app.Application;
+import android.os.AsyncTask;
+
 import androidx.annotation.NonNull;
 import androidx.paging.PagedList;
 
+import com.example.app_paging_boundry.api.RetrofitClient;
+import com.example.app_paging_boundry.db.UserDatabase;
 import com.example.app_paging_boundry.model.User;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class UserBoundaryCallback extends PagedList.BoundaryCallback<User> {
+    private static final int PER_PAGE = 5;
+    private Application application;
+    public UserBoundaryCallback(Application application) {
+        this.application = application;
+    }
 
     // 時機：若資料庫沒有數據時
     // onZeroItemsLoaded 沒有 Item 有數據時呼叫這個方法，通常是第一次進入列表時調用。
@@ -20,5 +36,34 @@ public class UserBoundaryCallback extends PagedList.BoundaryCallback<User> {
     @Override
     public void onItemAtEndLoaded(@NonNull User itemAtEnd) {
         super.onItemAtEndLoaded(itemAtEnd);
+    }
+
+    // 當資料表沒有數據時要透過 retrofit 加載數據
+    private void getFirstPage() {
+        int since = 0;
+        RetrofitClient.getInstance()
+                .getApi()
+                .getUsers(since, PER_PAGE)
+                .enqueue(new Callback<List<User>>() {
+                    @Override
+                    public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                        if(response.body() != null) {
+                            List<User> users = response.body(); // 網路爬來的資料
+                            addUsers(users); // 插入數據
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<User>> call, Throwable t) {
+
+                    }
+                });
+    }
+
+    // 插入數據
+    private void addUsers(List<User> users) {
+        AsyncTask.execute(() -> {
+            UserDatabase.getInstance(application).userDao().insertUsers(users);
+        });
     }
 }
